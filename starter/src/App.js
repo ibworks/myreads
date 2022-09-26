7ac168f20f7498cc15a042e41354397dc83531ac
ivan brown <259393@carmax.com>
2022-09-26 17:32:03 -0400

    adds routing

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,16 +1,15 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import "./App.css";
 import BookList from "./BookList";
-// import { Route, Routes } from "react-router-dom";
-// import BookSearch from "./BookSearch";
+import BookSearch from "./BookSearch";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <BookList />
-    // <Routes>
-    //   <Route exact path="/" element={<BookList />} />
-    //   <Route exact path="/search" element={<BookSearch />} />
-    // </Routes>
+
+    <Routes>
+      <Route exact path="/" element={<BookList />} />
+      <Route exact path="/search" element={<BookSearch />} />
+    </Routes>
    );
 }
 
